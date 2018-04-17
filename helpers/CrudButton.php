e15eca493c36a00b79d22f82210700ce96c68658
kozlovsv <kozlovsv78@gmail.com>
2018-04-17 09:00:15 +0500

    Небольшой рефакторинг. Добавлены параметры $url в кнопки, для изменения стандартного $url при необходимости

--- helpers/CrudButton.php
+++ helpers/CrudButton.php
@@ -12,29 +12,29 @@ class CrudButton
      * Рисует кнопку Назад, меняет заголовок в зависимости от того задан параметр returnUrl или нет
      * @param string $indexTitle текст кнопки если параметр returnUrl не задан
      * @param string $backTitle текст кнопки если параметр returnUrl задан
-     * @param string $defAction route по умолчанию
+     * @param array|string $defaultBackUrl ссылка по умолчанию
      * @param array $options HTML опции
      * @param string $iconName Имя Bootstrap3 иконки
      * @return string
      */
-    public static function backButton($indexTitle = 'Список', $backTitle = 'Назад', $defAction = 'index', $options = [], $iconName = 'arrow-left')
+    public static function backButton($indexTitle = 'Список', $backTitle = 'Назад', $defaultBackUrl = 'index', $options = [], $iconName = 'arrow-left')
     {
         Html::addCssClass($options, ['btn', 'btn-default']);
         $title = ReturnUrl::isSetReturnUrl() ? $backTitle : $indexTitle;
         if ($iconName) $title = Html::icon($iconName) . ' ' . $title;
-        return Html::a($title, ReturnUrl::getBackUrl($defAction), $options);
+        return Html::a($title, ReturnUrl::getBackUrl($defaultBackUrl), $options);
     }
 
     /**
      * Кнопка отмена
      * @param string $text
+     * @param array|string $defaultBackUrl - URL возврата по умолчанию
      * @return string
      */
-    public static function cancelButton($text = 'Отмена')
+    public static function cancelButton($text = 'Отмена', $defaultBackUrl = 'index')
     {
-        return Html::a($text, ReturnUrl::getBackUrl(), ['class' => 'btn btn-default form-cancel']);
+        return Html::a($text, ReturnUrl::getBackUrl($defaultBackUrl), ['class' => 'btn btn-default form-cancel']);
     }
-
     /**
      * Кнопка отмена
      * @param string $permissionCategory наименование раздела для определения разрешения
@@ -67,31 +67,33 @@ class CrudButton
      * @param string $permissionCategory Категория разрешения
      * @param int $id ID записи
      * @param bool $isModal Отображать форму редактирования в модальном окне
+     * @param array $url URL редактирования. Если пусто то используется URL по умолчанию
      * @return string
      */
-    public static function editButton($permissionCategory, $id, $isModal)
+    public static function editButton($permissionCategory, $id, $isModal, $url = [])
     {
         if (!ModelPermission::canUpdate($permissionCategory)) return '';
-        return Html::a(Html::icon('pencil'), ['update', 'id' => $id, ReturnUrl::REQUEST_PARAM_NAME => Url::to(['view', 'id' => $id])],
+        if (empty($url)) $url = ['update', 'id' => $id, ReturnUrl::REQUEST_PARAM_NAME => Url::to(['view', 'id' => $id])];
+        return Html::a(Html::icon('pencil'), $url,
             ['class' => 'btn btn-primary', 'data-modal' => $isModal ? 1 : 0]);
     }
 
     /**
      * @param string $permissionCategory Категория разрешения
      * @param int $id ID записи
+     * @param array $url URL редактирования. Если пусто то используется URL по умолчанию
      * @return string
      */
-    public static function deleteButton($permissionCategory, $id)
+    public static function deleteButton($permissionCategory, $id, $url = [])
     {
         if (!ModelPermission::canDelete($permissionCategory)) return '';
-
-        return Html::a(Html::icon('trash'), ['delete', 'id' => $id], [
+        if (empty($url)) $url = ['delete', 'id' => $id];
+        return Html::a(Html::icon('trash'), $url, [
             'class' => 'btn btn-danger pull-right',
             'data' => [
                 'confirm' => 'Удалить запись?',
                 'method' => 'post',
             ],
         ]);
-
     }
 }
