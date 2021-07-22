439f4730a0a1d10090bdb61c85b01bc56c8c82ee
kozlovsv78@gmail.com <kozlovsv78@gmail.com>
2021-07-22 17:41:18 +0500

    Проверка доступа на уровне приложения. Добавлена возможность добавлять в исключения контроллеры. Раньше была возможность добавлять только action.

--- components/RequestAccess.php
+++ components/RequestAccess.php
@@ -3,6 +3,7 @@
 namespace kozlovsv\crud\components;
 
 use Yii;
+use yii\base\Action;
 use yii\filters\AccessControl;
 
 /**
@@ -14,6 +15,11 @@ class RequestAccess extends AccessControl
      * @var array
      */
     public $allow_not_auth_actions = [];
+
+    /**
+     * @var array
+     */
+    public $allow_not_auth_controllers = [];
 
     /**
      * @var array
@@ -39,5 +45,20 @@ class RequestAccess extends AccessControl
             'actions' => $this->allow_not_auth_actions,
         ];
         parent::init();
+    }
+
+    /**
+     * This method is invoked right before an action is to be executed (after all possible filters.)
+     * You may override this method to do last-minute preparation for the action.
+     * @param Action $action the action to be executed.
+     * @return bool whether the action should continue to be executed.
+     */
+    public function beforeAction($action)
+    {
+        if (!empty($this->allow_not_auth_controllers)) {
+            $controllerId = Yii::$app->controller->id ?? '';
+            if (in_array($controllerId, $this->allow_not_auth_controllers)) return true;
+        }
+        return parent::beforeAction($action);
     }
 }
