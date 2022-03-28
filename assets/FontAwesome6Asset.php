9b9329d31f0dc53ae95aefe7eb492dc0e8d74b1a
kozlovsv78@gmail.com <kozlovsv78@gmail.com>
2022-03-28 21:23:03 +0500

    Added FontAwesome 6 Asset

---
+++ assets/FontAwesome6Asset.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace kozlovsv\crud\assets;
+
+use yii\web\AssetBundle;
+use yii\web\View;
+
+
+class FontAwesome6Asset extends AssetBundle
+{
+    /**
+     * @inheritdoc
+     */
+    public $css = [
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css'
+    ];
+}
