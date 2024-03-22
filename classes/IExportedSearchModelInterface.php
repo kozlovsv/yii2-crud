7e15cd3a818849010b7dfeaa5909147cc651dd13
Kozlov Sergey <kozlovsv78@gmail.com>
2024-03-22 19:14:30 +0500

    Рефакторинг. Поправлена иерархия интерфейсов IExportedSearchModelInterface

--- classes/IExportedSearchModelInterface.php
+++ classes/IExportedSearchModelInterface.php
@@ -1,10 +1,9 @@
 <?php
 namespace kozlovsv\crud\classes;
 
-use yii\data\ActiveDataProvider;
 use yii\db\ActiveQuery;
 
-interface IExportedSearchModelInterface
+interface IExportedSearchModelInterface extends ISearchModelInterface
 {
     /**
      * Creates data provider instance with search query applied
@@ -12,14 +11,6 @@ interface IExportedSearchModelInterface
      * @return ActiveQuery
      */
     public function searchQuery($params);
-
-
-    /**
-     * Поиск по параметрам фильтра
-     * @param array $params
-     * @return ActiveDataProvider
-     */
-    public function search($params = []);
 
     /**
      * @return array
